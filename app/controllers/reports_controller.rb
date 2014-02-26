4b444b5513603ded12595ff34195e675c8965f1a
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-02-26 16:05:44 +0630

    clean up general reports controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,13 +2,7 @@ class ReportsController < ApplicationController
   load_and_authorize_resource :report
 
   def index
-    if current_user.admin? then
-      @reports = Report.get_by_date(Date.today)
-    elsif current_user.member? then
-      @reports = Report.this_week.where(:user_id=> current_user).order(:report_date)
-    else
-      @reports = Report.all
-    end
+    @reports = Report.get_by_date(Date.today)
   end
 
   def show
@@ -19,20 +13,6 @@ class ReportsController < ApplicationController
     @report = Report.find(params[:id])
   end
 
-  def create
-    @reports = []
-    first = Date.today.beginning_of_week
-    last = Date.today.beginning_of_week+4
-    (first..last).each do |date|
-      @reports<<Report.create!(report_date: date,user_id: current_user.id)
-    end
-    respond_to do |format|
-      format.html do
-        redirect_to reports_path
-      end
-      format.js
-    end
-  end
 
   def new
     @report = Report.new
