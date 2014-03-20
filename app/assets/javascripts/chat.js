8d6054edbf1c59844c32f8c5ccf3b25ffeb27fb5
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-03-20 11:47:27 +0630

    fixed dynamic event binding for group#create.js.erb

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,16 +1,21 @@
 $(function(){
 $('#new_message').hide();
-  $('.use_group').on('click',function(){
-   var gid = $(this).data('gid');
-  $('#new_message').show();
-    $('#message_group_id').attr('value',gid);
-    $('.tmgt-chat-box').text('Loading');
-      $.ajax({
-          url: '/messages',
-          data: {group_id: gid},
-          dataType: "script"
+    jQuery.fn.groupClick = function(){
+      this.on('click',function(){
+          var gid = $(this).data('gid');
+          $('#new_message').show();
+          $('#message_group_id').attr('value',gid);
+          $('.tmgt-chat-box').text('Loading');
+          $.ajax({
+              url: '/messages',
+              data: {group_id: gid},
+              dataType: "script"
+          });
+          var t = $(this).find('.name').text();
+          $('#current_group').text(t);
       });
-    var t = $(this).find('.name').text();
-    $('#current_group').text(t);
-});
+        return this;
+  };
+    $('.use_group').groupClick();
+
 });
